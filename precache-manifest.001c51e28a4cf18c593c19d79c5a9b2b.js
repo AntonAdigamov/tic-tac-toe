09cbba9228e188120a7e1c22c3849fadd5e865fd
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2019-02-01 20:39:52 +0100

    Updates

---
+++ precache-manifest.001c51e28a4cf18c593c19d79c5a9b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe400e5019f42bfd694",
+    "url": "/tic-tac-toe/static/css/main.08f826ea.chunk.css"
+  },
+  {
+    "revision": "ffe400e5019f42bfd694",
+    "url": "/tic-tac-toe/static/js/main.ffe400e5.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "bcb8f42bd01872c56900492bf552d955",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
